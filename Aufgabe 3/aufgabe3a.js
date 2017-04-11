c578d125b90fc0229d3fce1bd05e92d853dd0cd1
RUDY <RUDY@RUDY-TOSH>
2017-04-11 15:30:34 +0200

    verbesserung aufgabe3a

--- Aufgabe 3/aufgabe3a.js
+++ Aufgabe 3/aufgabe3a.js
@@ -8,8 +8,7 @@ Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde n
 document.addEventListener('DOMContentLoaded', function () {
     var row = 1;
     var rice = 1;
-    var backgroundcolor;
-    var color;
+    var klasse;
     for (let i = 0; i < 64; i++) {
         //zeilencounter
         if (i % 8 == 0) {
@@ -19,55 +18,47 @@ document.addEventListener('DOMContentLoaded', function () {
         if (row % 2 == 0) {
             //wei�
             if (i % 2 == 0) {
-                backgroundcolor = "#FFFFFF";
-                color = "#000000";
+                klasse = "white";
             }
             else {
-                backgroundcolor = "#000000";
-                color = "#FFFFFF";
+                klasse = "black";
             }
         }
         else {
             //schwarz
             if (i % 2 == 0) {
-                backgroundcolor = "#000000";
-                color = "#FFFFFF";
+                klasse = "black";
             }
             else {
-                backgroundcolor = "#FFFFFF";
-                color = "#000000";
+                klasse = "white";
             }
         }
         //div erstellen
-        placeDiv(color, backgroundcolor);
+        placeDiv(klasse, i);
         //Reisk�rner vermehren
         rice = rice * 2;
     }
-    function placeDiv(color, backgroundcolor) {
+    function placeDiv(klasse, i) {
         let div = document.createElement("div");
         div.textContent = rice.toString();
         document.body.appendChild(div);
+        if (i < 8)
+            div.addEventListener("click", selection);
         let s = div.style;
-        s.backgroundColor = backgroundcolor;
-        s.color = color;
+        div.classList.toggle(klasse);
         s.width = "100px";
         s.height = "100px";
     }
-});
-//Aufgabe 3a
-var felder = document.getElementsByTagName("div");
-for (let i= 0; i< 9; i++) {
-     felder[i].addEventListener("click", selection)
-    }
-function selection (){
-    
-    if (felder.style.backgroundColor = "#FFFFFF" || "#000000"){
-        felder.className = "selected";
-        }
-    if (felder.style.backgroundColor = "red"){
-        felder.className = "notselected";
-       }
+    //Aufgabe 3a
+    function selection(_event) {
+        let select = _event.target;
+        select.classList.toggle("selected");
+        /*if (felder.style.backgroundColor = "#FFFFFF" || "#000000"){
+            felder.className = "selected";
+            }
+        if (felder.style.backgroundColor = "red"){
+            felder.className = "notselected";
+           }*/
     }
 });
-
 //# sourceMappingURL=aufgabe3a.js.map
--- Aufgabe 3/aufgabe3a.ts
+++ Aufgabe 3/aufgabe3a.ts
@@ -9,12 +9,11 @@ document.addEventListener('DOMContentLoaded', function() {
 
     var row: number = 1;
     var rice: number = 1;
-    var backgroundcolor: string;
-    var color: string;
+    var klasse: string;
     for (let i: number = 0; i < 64; i++) {
-    //zeilencounter
+        //zeilencounter
         if (i % 8 == 0) {
-           row = row + 1;
+            row = row + 1;
         }
 
         //gerade zeilen
@@ -22,14 +21,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
             //weiß
             if (i % 2 == 0) {
-                backgroundcolor = "#FFFFFF";
-                color = "#000000";
+                klasse = "white";
             }
 
             //schwarz
             else {
-                backgroundcolor = "#000000";
-                color = "#FFFFFF";
+                klasse = "black";
             }
         }
 
@@ -38,50 +35,49 @@ document.addEventListener('DOMContentLoaded', function() {
 
             //schwarz
             if (i % 2 == 0) {
-                backgroundcolor = "#000000";
-                color = "#FFFFFF";
+                klasse = "black";
             }
 
             //weiß
             else {
-                backgroundcolor = "#FFFFFF";
-                color = "#000000";
+                klasse = "white";
             }
         }
 
         //div erstellen
-        placeDiv(color, backgroundcolor);
+        placeDiv(klasse, i);
         //Reiskörner vermehren
         rice = rice * 2;
 
     }
 
-    function placeDiv(color: string, backgroundcolor: string): void {
+    function placeDiv(klasse: string, i: number): void {
         let div: HTMLDivElement = document.createElement("div");
         div.textContent = rice.toString();
         document.body.appendChild(div);
 
+        if (i < 8)
+            div.addEventListener("click", selection);
+        
         let s: CSSStyleDeclaration = div.style;
-        s.backgroundColor = backgroundcolor;
-        s.color = color;
+        div.classList.toggle(klasse);
         s.width = "100px";
         s.height = "100px";
     }
 
 
-//Aufgabe 3a
+    //Aufgabe 3a
 
-var felder: NodeListOf<HTMLElement> = document.getElementsByTagName("div");
-for (let i= 0; i< 9; i++) {
-     felder[i].addEventListener("click", selection)
+   
+    function selection(_event: MouseEvent): void {
+        let select: HTMLElement = <HTMLElement>_event.target;
+
+        select.classList.toggle("selected");
+        /*if (felder.style.backgroundColor = "#FFFFFF" || "#000000"){
+            felder.className = "selected";
+            }
+        if (felder.style.backgroundColor = "red"){
+            felder.className = "notselected";
+           }*/
     }
-function selection (){
-    
-    if (felder.style.backgroundColor = "#FFFFFF" || "#000000"){
-        felder.className = "selected";
-        }
-    if (felder.style.backgroundColor = "red"){
-        felder.className = "notselected";
-       }
-    };
 });
