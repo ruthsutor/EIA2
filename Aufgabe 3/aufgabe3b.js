ebabcb26387d120e6770b411493e17d0f9988a2d
RUDY <RUDY@RUDY-TOSH.hfu-open.fu.hs-furtwangen.de>
2017-04-20 11:46:02 +0200

    verbessert

--- Aufgabe 3/aufgabe3b.js
+++ Aufgabe 3/aufgabe3b.js
@@ -1,16 +1,20 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var karten = ["Kreuz 7", "Kreuz 8", "Kreuz 9", "Kreuz 10", "Kreuz Bube", "Kreuz Dame", "Kreuz K�nig", "Kreuz As",
+var maumau;
+(function (maumau) {
+    let karten = ["Kreuz 7", "Kreuz 8", "Kreuz 9", "Kreuz 10", "Kreuz Bube", "Kreuz Dame", "Kreuz K�nig", "Kreuz As",
         "Herz 7", "Herz 8", "Herz 9", "Herz 10", "Herz Bube", "Herz Dame", "Herz K�nig", "Herz As",
         "Pik 7", "Pik 8", "Pik 9", "Pik 10", "Pik Bube", "Pik Dame", "Pik K�nig", "Pik As",
         "Karo 7", "Karo 8", "Karo 9", "Karo 10", "Karo Bube", "Karo Dame", "Karo K�nig", "Karo As"];
-    var hand = [];
-    var stapelkarten = karten.length;
-    var handkarten = 0;
-    document.getElementById("stapel").addEventListener("click", newCard);
+    let hand = [];
+    let stapelkarten = karten.length;
+    let handkarten = 0;
+    window.onload = init;
+    function init() {
+        document.getElementById("stapel").addEventListener("click", newCard);
+    }
     function newCard() {
-        if (stapelkarten > 0 && handkarten < 5) {
-            var zufall = Math.floor((Math.random() * stapelkarten) + 0); //Zufallsgenerator
-            var gezogen = karten[zufall]; //die gezogene Karte
+        if (karten.length > 0 && handkarten < 5) {
+            let zufall = Math.round(Math.random() * (karten.length - 1)); //Zufallsgenerator
+            let gezogen = karten[zufall]; //die gezogene Karte
             karten.splice(zufall, 1); //Karte aus Array entfernen
             handkarten = handkarten + 1; //eine Karte mehr auf der Hand
             let div = document.createElement("div"); //Handkarte erstellen
@@ -23,10 +27,10 @@ document.addEventListener('DOMContentLoaded', function () {
             div.style.border = "2px #000000 solid";
             div.style.textAlign = "center";
             div.style.display = "inline";
-            hand.push(karten[zufall]); //gezogene Karte wird Hand Array hinzugef�gt
+            hand.push(gezogen); //gezogene Karte wird Hand Array hinzugef�gt
             div.addEventListener("click", toAblage);
         }
-        if (stapelkarten == 0) {
+        if (karten.length == 0) {
             document.getElementById("stapel").textContent = "Karten leer";
         }
     }
@@ -41,5 +45,5 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         }
     }
-});
+})(maumau || (maumau = {}));
 //# sourceMappingURL=aufgabe3b.js.map
--- Aufgabe 3/aufgabe3b.ts
+++ Aufgabe 3/aufgabe3b.ts
@@ -1,22 +1,24 @@
-document.addEventListener('DOMContentLoaded', function() {
+namespace maumau {
 
-    var karten = ["Kreuz 7", "Kreuz 8", "Kreuz 9", "Kreuz 10", "Kreuz Bube", "Kreuz Dame", "Kreuz König", "Kreuz As",
+    let karten: string[] = ["Kreuz 7", "Kreuz 8", "Kreuz 9", "Kreuz 10", "Kreuz Bube", "Kreuz Dame", "Kreuz König", "Kreuz As",
         "Herz 7", "Herz 8", "Herz 9", "Herz 10", "Herz Bube", "Herz Dame", "Herz König", "Herz As",
         "Pik 7", "Pik 8", "Pik 9", "Pik 10", "Pik Bube", "Pik Dame", "Pik König", "Pik As",
         "Karo 7", "Karo 8", "Karo 9", "Karo 10", "Karo Bube", "Karo Dame", "Karo König", "Karo As"];
-    var hand: string[] = [];
-    var stapelkarten: number = karten.length;
-    var handkarten: number = 0;
-    document.getElementById("stapel").addEventListener("click", newCard);
+    let hand: string[] = [];
+    let stapelkarten: number = karten.length;
+    let handkarten: number = 0;
+    window.onload = init;
+    function init(): void {
+        document.getElementById("stapel").addEventListener("click", newCard);
 
-
-    function newCard() {
-        if (stapelkarten > 0 && handkarten < 5) {
-            var zufall = Math.floor((Math.random() * stapelkarten) + 0); //Zufallsgenerator
-            var gezogen = karten[zufall]; //die gezogene Karte
+    }
+    function newCard(): void {
+        if (karten.length > 0 && handkarten < 5) {
+            let zufall: number = Math.round(Math.random() * (karten.length - 1)); //Zufallsgenerator
+            let gezogen: string = karten[zufall]; //die gezogene Karte
             karten.splice(zufall, 1); //Karte aus Array entfernen
             handkarten = handkarten + 1; //eine Karte mehr auf der Hand
-            let div = document.createElement("div"); //Handkarte erstellen
+            let div: HTMLElement = document.createElement("div"); //Handkarte erstellen
             document.getElementById("hand").appendChild(div);
             div.textContent = gezogen;
             div.style.width = "100px";
@@ -26,10 +28,10 @@ document.addEventListener('DOMContentLoaded', function() {
             div.style.border = "2px #000000 solid";
             div.style.textAlign = "center";
             div.style.display = "inline";
-            hand.push(karten[zufall]); //gezogene Karte wird Hand Array hinzugefügt
+            hand.push(gezogen); //gezogene Karte wird Hand Array hinzugefügt
             div.addEventListener("click", toAblage);
         }
-        if (stapelkarten == 0){
+        if (karten.length == 0) {
             document.getElementById("stapel").textContent = "Karten leer";
         }
     }
@@ -39,11 +41,11 @@ document.addEventListener('DOMContentLoaded', function() {
         choosenCard.parentNode.removeChild(choosenCard); //erstelltes Div löschen
         handkarten = handkarten - 1;
         document.getElementById("ablage").textContent = choosenCard.textContent;
-        for (let i = 0; i < hand.length; i++) {
+        for (let i: number = 0; i < hand.length; i++) {
             if (choosenCard.textContent == hand[i]) { //guckt welche Karte angeklickt wurde
                 hand.splice(i, 1); // entfernt die Karte aus dem Array
             }
         }
     }
-});
+}
 
