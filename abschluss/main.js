b9a026b7552baaa1e81395198037928ee8708852
RUDY <RUDY@RUDY-TOSH>
2017-07-19 13:55:29 +0200

    works

--- abschluss/main.js
+++ abschluss/main.js
@@ -6,37 +6,85 @@ Datum: 21/07/17
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
 */
 window.addEventListener("load", init);
-let allfields = [];
+let allpictures = [];
+let allbackgrounds = [];
+let shownpictures = [];
 function init() {
     document.getElementById("start").addEventListener("click", newGame);
 }
 function newGame() {
     document.getElementById("start").style.visibility = "hidden";
-    createFields();
+    document.getElementById("buttondiv").style.zIndex = "0";
+    createPictures();
 }
-function createFields() {
+function createPictures() {
     for (let i = 0; i < 12; i++) {
         for (let u = 0; u < 2; u++) {
             let source = "images/pair" + i + ".jpg";
             let pairnumber = i;
-            let field = { src: source, pair: pairnumber };
-            allfields.push(field);
+            let pic = { src: source, pair: pairnumber };
+            allpictures.push(pic);
         }
     }
     placeDivs();
 }
 function placeDivs() {
     for (let i = 0; i < 24; i++) {
-        let random = Math.round(Math.random() * allfields.length);
-        let bigdiv = document.getElementById("fielddiv");
+        let random = Math.round(Math.random() * (allpictures.length - 1) + 0);
+        let picdiv = document.getElementById("picdiv");
+        let backgrounddiv = document.getElementById("background");
         let div = document.createElement("div");
+        let background = document.createElement("div");
         let img = document.createElement("img");
-        img.src = allfields[random].src;
+        img.src = allpictures[random].src;
         img.style.width = "50px";
         img.style.height = "50px";
         div.appendChild(img);
-        bigdiv.appendChild(div);
-        allfields.splice(random, 1);
+        picdiv.appendChild(div);
+        background.id = String(i);
+        background.addEventListener("click", showPicture);
+        backgrounddiv.appendChild(background);
+        allbackgrounds.push(backgrounddiv);
+        allpictures.splice(random, 1);
     }
+}
+function showPicture(_event) {
+    if (shownpictures.length == 2) {
+        coverPictures();
+        clearShownPictures();
+    }
+    let selected = _event.target;
+    selected.style.visibility = "hidden";
+    let fieldnumber = parseInt(selected.id);
+    shownpictures.push(fieldnumber);
+    if (shownpictures.length == 2) {
+        checkIfPair();
+    }
+}
+function checkIfPair() {
+    let allimgs = document.getElementsByTagName("img");
+    let img1 = allimgs[shownpictures[0]];
+    let img2 = allimgs[shownpictures[1]];
+    if (img1.src == img2.src) {
+        removePair();
+    }
+}
+function removePair() {
+    let alldivs = document.getElementsByTagName("div");
+    let div1 = alldivs[shownpictures[0] + 2];
+    let div2 = alldivs[shownpictures[1] + 2];
+    div1.style.backgroundColor = "#ffffff";
+    div1.removeEventListener("click", showPicture);
+    div2.style.backgroundColor = "#ffffff";
+    div2.removeEventListener("click", showPicture);
+}
+function coverPictures() {
+    let alldivs = document.getElementsByTagName("div");
+    for (let i = 0; i < alldivs.length; i++) {
+        alldivs[i].style.visibility = "visible";
+    }
+}
+function clearShownPictures() {
+    shownpictures.length = 0;
 }
 //# sourceMappingURL=main.js.map
--- abschluss/main.ts
+++ abschluss/main.ts
@@ -7,40 +7,88 @@ Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde n
 */
 
 window.addEventListener("load", init);
-let allfields: Field[] = [];
+let allpictures: Picture[] = [];
+let allbackgrounds: HTMLDivElement[] = [];
+let shownpictures: number[] = [];
 function init(): void {
     document.getElementById("start").addEventListener("click", newGame);
 }
 function newGame(): void {
     document.getElementById("start").style.visibility = "hidden";
-    createFields();
+    document.getElementById("buttondiv").style.zIndex = "0";
+    createPictures();
 }
-function createFields(): void {
+function createPictures(): void {
     for (let i: number = 0; i < 12; i++) {
         for (let u: number = 0; u < 2; u++) {
             let source: string = "images/pair" + i + ".jpg";
             let pairnumber: number = i;
-            let field: Field = {src: source, pair: pairnumber};
-            allfields.push(field);
+            let pic: Picture = { src: source, pair: pairnumber };
+            allpictures.push(pic);
         }
     }
     placeDivs();
 }
 function placeDivs(): void {
     for (let i: number = 0; i < 24; i++) {
-        let random: number = Math.round(Math.random() * allfields.length);
-        let bigdiv: HTMLDivElement = <HTMLDivElement>document.getElementById("fielddiv");
+        let random: number = Math.round(Math.random() * (allpictures.length - 1) + 0);
+        let picdiv: HTMLDivElement = <HTMLDivElement>document.getElementById("picdiv");
+        let backgrounddiv: HTMLDivElement = <HTMLDivElement>document.getElementById("background");
         let div: HTMLDivElement = document.createElement("div");
+        let background: HTMLDivElement = document.createElement("div");
         let img: HTMLImageElement = document.createElement("img");
-        img.src = allfields[random].src;
+        img.src = allpictures[random].src;
         img.style.width = "50px";
         img.style.height = "50px";
         div.appendChild(img);
-        bigdiv.appendChild(div);
-        allfields.splice(random, 1);
-        }
+        picdiv.appendChild(div);
+        background.id = String(i);
+        background.addEventListener("click", showPicture);
+        backgrounddiv.appendChild(background);
+        allbackgrounds.push(backgrounddiv);
+        allpictures.splice(random, 1);
+    }
 }
-interface Field {
+function showPicture(_event: MouseEvent): void {
+    if (shownpictures.length == 2) {
+        coverPictures();
+        clearShownPictures();
+    }
+    let selected: HTMLElement = <HTMLElement>_event.target;
+    selected.style.visibility = "hidden";
+    let fieldnumber: number = parseInt(selected.id);
+    shownpictures.push(fieldnumber);
+    if (shownpictures.length == 2) {
+        checkIfPair();
+    }
+}
+function checkIfPair (): void {
+    let allimgs: NodeListOf<HTMLImageElement> = document.getElementsByTagName("img");
+    let img1: HTMLImageElement = allimgs[shownpictures[0]];
+    let img2: HTMLImageElement = allimgs[shownpictures[1]];
+    if (img1.src == img2.src) {
+        removePair();
+        }
+    }
+function removePair(): void {
+    let alldivs: NodeListOf<HTMLDivElement> = document.getElementsByTagName("div");
+    let div1: HTMLDivElement = alldivs[shownpictures[0] + 2];
+    let div2: HTMLDivElement = alldivs[shownpictures[1] + 2];
+    div1.style.backgroundColor = "#ffffff";
+    div1.removeEventListener("click", showPicture);
+    div2.style.backgroundColor = "#ffffff";
+    div2.removeEventListener("click", showPicture);
+}
+function coverPictures(): void {
+    let alldivs: NodeListOf<HTMLDivElement> = document.getElementsByTagName("div");
+    for (let i: number = 0; i < alldivs.length; i++) {
+        alldivs[i].style.visibility = "visible";
+    }
+}
+function clearShownPictures(): void {
+    shownpictures.length = 0;
+}
+interface Picture {
     src: string;
     pair: number;
 } 
